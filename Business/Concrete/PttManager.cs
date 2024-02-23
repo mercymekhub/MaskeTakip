4cb2f1f656852da59575bbef154545e8af784a2a
Özgür TOPCU <topcuozgur@gmail.com>
2024-02-24 01:44:14 +0300

    Add project files.

---
+++ Business/Concrete/PttManager.cs
@@ -0,0 +1,30 @@
+﻿using Business.Abstract;
+using Entites.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class PttManager : ISupplierService
+    {
+        private IApplicantService _applicantService;
+        public PttManager(IApplicantService applicantService)
+        {
+            _applicantService = applicantService;
+        }
+        public void GiveMask(Person person)
+        {
+            //PttManager pttManager = new PttManager(new ForeignerManager());
+            //PersonManager personManager = new PersonManager();
+            if (_applicantService.CheckPerson(person))
+            {
+                Console.WriteLine(person.FirstName + " için maske verildi");
+            }
+            else
+            {
+                Console.WriteLine(person.FirstName + " için maske VERİLEMEDİ");
+            }
+        }
+    }
+}
---
+++ Workaround/Program.cs
@@ -0,0 +1,27 @@
+﻿using Business.Concrete;
+using Entites.Concrete;
+using System;
+
+namespace Workaround
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            SelamVer();
+
+            PttManager pttManager = new PttManager(new PersonManager());
+            pttManager.GiveMask(new Person {  DateOfBirthYear=1985,
+             FirstName="isim", LastName = "soyisim", NationalIdentity = 123 });
+
+            Console.ReadLine();
+        }
+
+        static void SelamVer()
+        {
+            Console.WriteLine("Merhaba");
+        }
+    }
+}
